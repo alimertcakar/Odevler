ed9fb81eb0c4d7e48d7c540c469c36d7df410bb9
Ali Mert <mail@alimert.net>
2020-06-17 10:08:16 +0300

    delete duplicate files

--- Web Editörü Yollandı/leetcode_1389.js
+++
@@ -1,8 +0,0 @@
-var createTargetArray = function (nums, index) {
-    let output = [];
-    index.forEach((item, index) => {
-        output.splice(item, 0, nums[index]) // splice kullanarak insert yapıyoruz
-    });
-    return output;
-};
-
